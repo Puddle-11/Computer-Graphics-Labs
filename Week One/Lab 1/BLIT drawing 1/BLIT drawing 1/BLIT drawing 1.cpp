b2a15524ac653891a71764c7923f1afe384b4fb9
Puddle <Puddleglumstudio@gmail.com>
2024-10-29 21:43:54 -0400

    Added week 1 lab 1 resources

---
+++ Week One/Lab 1/BLIT drawing 1/BLIT drawing 1/BLIT drawing 1.cpp
@@ -0,0 +1,10 @@
+// BLIT drawing 1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include "RasterSurface.h"
+#include "XTime.h"
+int main()
+{
+    std::cout << "Hello World!\n";
+}
